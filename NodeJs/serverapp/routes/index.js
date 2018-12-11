54535ec00f869acb597302e50711a35b2980d794
davidrubio2 <davidrubio2@gmail.com>
2018-12-10 23:48:31 -0700

    se corrigio los que son post y get

--- NodeJs/serverapp/routes/index.js
+++ NodeJs/serverapp/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 router.post('/api/NuevaTarea', db.funNuevaTarea);
-router.post('/api/ConsultarTodosResponsables', db.funConsultarTodosResponsables);
-router.post('/api/ConsultarTodasCategorias', db.funConsultarTodasCategorias);
-router.get('/api/ConsultarPorFiltros', db.funConsultarPorFiltros);
+router.get('/api/ConsultarTodosResponsables', db.funConsultarTodosResponsables);
+router.get('/api/ConsultarTodasCategorias', db.funConsultarTodasCategorias);
+router.post('/api/ConsultarPorFiltros', db.funConsultarPorFiltros);
 router.post('/api/NuevaSubTarea', db.funNuevaSubTarea);
 
 
