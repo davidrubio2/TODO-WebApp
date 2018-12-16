abd3391518eed5973ced5fc9424914b918d2d4af
davidrubio2 <davidrubio2@gmail.com>
2018-12-16 11:52:44 -0700

    se actualizaron las tablas

--- Base de datos/PruebasTablas.sql
+++ Base de datos/PruebasTablas.sql
@@ -5,15 +5,14 @@ id_responsable int not null,
 tarea_fecha_de_inicio timestamp  not null,
 tarea_fecha_de_fin timestamp  not null,
 tarea_porcentaje float default 0.0,
-categoria_id int  not null,
-id_sub_tareas int default 0
+categoria_id int  not null
 );
 
 drop table tarea;
 delete from tarea
 insert into tarea(tarea_titulo,id_responsable,
-tarea_fecha_de_inicio,tarea_fecha_de_fin, tarea_porcentaje,categoria_id,id_sub_tareas) values('Cortar Verdura',
-'2','2018-12-05','2018-12-08','0.1','2','1')
+tarea_fecha_de_inicio,tarea_fecha_de_fin, tarea_porcentaje,categoria_id) values('Cortar Verdura',
+'2','2018-12-05','2018-12-08','0.1','2')
 -------------------------------------------------------------------------------------------
 CREATE TABLE responsable(
 responsable_id SERIAL PRIMARY KEY,
@@ -44,10 +43,11 @@ CREATE TABLE sub_tarea(
 sub_tarea_id SERIAL PRIMARY KEY,
 id_responsable int,
 sub_tarea_nombre varchar(20),
-sub_tarea_fecha_de_vencimiento timestamp
+sub_tarea_fecha_de_vencimiento timestamp,
+tarea_id int  not null
 );
 delete from sub_Tarea;
 drop table sub_Tarea;
-insert into sub_tarea(id_responsable,sub_tarea_nombre, sub_tarea_fecha_de_vencimiento) values('1',
-'calar','2018-12-05');
+insert into sub_tarea(id_responsable,sub_tarea_nombre, sub_tarea_fecha_de_vencimiento,tarea_id) values('1',
+'calar','2018-12-05','1');
 
