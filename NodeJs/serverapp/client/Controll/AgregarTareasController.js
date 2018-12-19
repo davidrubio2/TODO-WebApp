138c31a2462818308de3c828c83cf603a333cba6
davidrubio2 <davidrubio2@gmail.com>
2018-12-19 01:08:03 -0700

    se actualizo

--- NodeJs/serverapp/client/Controll/AgregarTareasController.js
+++ NodeJs/serverapp/client/Controll/AgregarTareasController.js
@@ -1,16 +1,37 @@
 var AgregarTareaSession = angular.module("AgregarTareaSession", ['AgregarTareaSessionAppService']);
 AgregarTareaSession.controller("AgregarTareaSessionController", ['$scope', 'AgregarTareaSessionService', function ($scope, AgregarTareaSessionService) {
+       
 
         $scope.AgregarTareaSessionAppService = []
-        
+        var dataSubTarea = [] ;
         AgregarTareaSessionService.GetConsultarTodasCategorias().then(function (response) {
-                $scope.Categoria = response.data.data;
+                $scope.Categorias = response.data.data;
+                $scope.Categoria = $scope.Categorias[0];
         });
 
         AgregarTareaSessionService.GetConsultarResponsables().then(function (response) {
                 $scope.Responsables = response.data.data;
+                $scope.Responsable = $scope.Responsables[0];
         });
-
+        $scope.AgregarTarea = function () {
+                var data = {
+                    "TareaTitulo": $scope.titulo,
+                    "IdResponsable": $scope.Responsable.val_id,
+                    "FechaInicio": $scope.FechaDeinicio,
+                    "FechaFin": $scope.FechadeFin,
+                    "Porcentaje": $scope.PorcentajedeAvance,
+                    "IdCategoria": $scope.Categoria.val_id,
+                }
+                AgregarTareaSessionService.InsertarTarea(data).then(function (response) {
+                    $scope.AgregarTareaSessionAppService.push(response.data)
+                    
+                });
+                
+             }
+             $scope.AgregarSubTarea = function () {
+                dataSubTarea.push(  $scope.Responsable.val_id);
+             
+        }
 
 
 
--- NodeJs/serverapp/client/Servic/AgregarTareasServices.js
+++ NodeJs/serverapp/client/Servic/AgregarTareasServices.js
@@ -1,19 +1,24 @@
 angular.module('AgregarTareaSessionAppService', [])
         .factory('AgregarTareaSessionService', ['$http', function ($http) {
-            var handler = {
+                var handler = {
 
-                GetConsultarResponsables: function () {
-                        var url = 'api/ConsultarTodosResponsables'
-                        return $http.get(url)
+                        GetConsultarResponsables: function () {
+                                var url = 'api/ConsultarTodosResponsables'
+                                return $http.get(url)
 
-                },
-                
-                GetConsultarTodasCategorias: function () {
-                        var url = 'api/ConsultarTodasCategorias'
-                        return $http.get(url)
+                        },
 
-                },
-        }
+                        GetConsultarTodasCategorias: function () {
+                                var url = 'api/ConsultarTodasCategorias'
+                                return $http.get(url)
+
+                        },
+                        InsertarTarea: function (data) {
+                                var url = 'api/NuevaTarea'
+                                return $http.post(url, data)
+
+                        },
+                }
 
                 return handler;
         }]);	
